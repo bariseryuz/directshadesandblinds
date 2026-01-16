1acc91b768377f6b847d5a5930f450f110d1ad99
bariseryuz <fa-der-19981@hotmail.com>
2026-01-16 03:01:12 +0300

    Replace about.mp4 with aboutt.mp4

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -91,7 +91,7 @@ export default function AboutPage() {
         playsInline
         className="fixed inset-0 w-full h-full object-cover opacity-40"
       >
-        <source src="/about.mp4" type="video/mp4" />
+        <source src="/aboutt.mp4" type="video/mp4" />
       </video>
 
       {/* Gradient Overlay */}
